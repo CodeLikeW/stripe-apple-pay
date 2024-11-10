fd2951066c190ccf290c6d5148f5422de997f384
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2024-11-10 10:19:11 +0100

    Add StripeCore dependency

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,6 +10,9 @@ let package = Package(
         .library(
             name: "StripeApplePay",
             targets: ["StripeApplePay"]),
+    ],
+    dependencies: [
+        .package(url: "file:///Users/balazs/dev/ios/StripeCore", from: "24.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
